e80a957d869803fb685b921f328b1939fb85a2d3
MarollaVictor <marollavictorsedky@gmail.com>
2025-04-26 01:20:31 +0300

    add 2 float numbers

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def add(a: float, b: float) -> float:
+    """
+    adds two float numbers and returns the result
+
+    Args:
+        a (float): The first float number
+        b (float): The second float number
+
+    Returns:
+        float: The sum of the two float numbers
+    """
+    return a + b
