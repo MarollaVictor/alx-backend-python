6e558176a2a604209d3513a2e7e5b787db00110a
MarollaVictor <marollavictorsedky@gmail.com>
2025-04-26 02:08:53 +0300

    string concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def concat(str1: str, str2: str) -> str:
+    """
+    concatenates two strings and returns the result
+
+    Args:
+        str1 (str): The first string
+        str2 (str): The second string
+
+    Returns:
+        str: The concatenation of the two strings
+    """
+    return str1 + str2
