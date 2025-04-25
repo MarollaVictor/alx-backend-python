f0ab83fa7a5f2035f6141beb917889a0ad7374d2
MarollaVictor <marollavictorsedky@gmail.com>
2025-04-26 02:39:25 +0300

    python3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def to_str(n: float) -> str:
+    """
+    converts a float number to a string and returns the result
+
+    Args:
+        n (float): The float number
+
+    Returns:
+        str: The string representation of the float number
+    """
+    return str(n)
